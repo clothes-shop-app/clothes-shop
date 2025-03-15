617b60e2cc55aa913d3aeb1b285eb371ef2eb443
Alnur Zhanibek <alnur.zhanibek@indriver.com>
2025-03-15 17:31:17 +0500

    feat: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello, world!</p>
+    <div className="my-8 space-y-4">
+      <Link href="/" className="text-sm text-gray-700">
+        Главная
+      </Link>
+      <h2 className="text-center text-xl font-bold">Каталог</h2>
+      <div className="flex border-t border-b">
+        <div className="w-[300px] p-4">
+          <p>Фильтры</p>
+        </div>
+        <div className="grow border-l h-[1000px]"></div>
+      </div>
     </div>
   )
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,1 +1,34 @@
-export const Header = () => {}
+import Link from 'next/link'
+import { UserCircleIcon, ShoppingCartIcon, BellIcon } from 'lucide-react'
+import { Playfair_Display } from 'next/font/google'
+
+const playfairDisplay = Playfair_Display({
+  variable: '--font-playfair-display',
+  subsets: ['latin'],
+  weight: ['400']
+})
+
+export const Header = () => {
+  return (
+    <header className="border-b border-gray-500">
+      <div className="flex justify-between items-end container mx-auto pt-8 pb-4">
+        <nav className="flex gap-8 flex-1">
+          <Link href="/">Каталог</Link>
+          <Link href="#">Новинки</Link>
+          <Link href="#">Скидки</Link>
+          <Link href="#">О нас</Link>
+        </nav>
+        <div className="flex-1 flex justify-center">
+          <Link href="/">
+            <h1 className={`text-4xl ${playfairDisplay.className}`}>Clothes</h1>
+          </Link>
+        </div>
+        <div className="flex gap-4 flex-1 justify-end">
+          <UserCircleIcon width={20} height={20} />
+          <ShoppingCartIcon width={20} height={20} />
+          <BellIcon width={20} height={20} />
+        </div>
+      </div>
+    </header>
+  )
+}
